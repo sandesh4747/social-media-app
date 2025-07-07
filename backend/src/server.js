8de1d1a61502860f74d326256e70326e22f54c85
sandesh4747 <sangitthapa328@gmail.com>
2025-07-07 10:22:42 +0545

    changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -25,9 +25,9 @@ app.use("/api/post", postRoutes);
 app.use("/api/user", userRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"))
   );
 }
 

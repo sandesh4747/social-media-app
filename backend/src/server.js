785c63162cf9d7505712209abfe022364bbb6f5b
sandesh4747 <sangitthapa328@gmail.com>
2025-07-07 09:09:10 +0545

    changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,10 +12,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: [
-      "https://social-media-app-two-self.vercel.app",
-      process.env.CLIENT_URL,
-    ],
+    origin: ["https://social-media-app-two-self.vercel.app"],
     credentials: true,
   })
 );
--- frontend/src/app/mainApi.js
+++ frontend/src/app/mainApi.js
@@ -1,8 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 // export const baseUrl = "http://localhost:5002/api";
-export const baseUrl =
-  process.env.REACT_APP_API_URL || "http://localhost:5002/api";
+export const baseUrl = process.env.REACT_APP_API_URL;
 
 export const mainApi = createApi({
   reducerPath: "mainApi",
